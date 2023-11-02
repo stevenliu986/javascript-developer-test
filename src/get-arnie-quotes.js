a9192ecf845b77fe9556e906c763752a72720ce9
StevenLiu <stevenliu986@gmail.com>
2023-11-02 18:46:08 +1100

    implement function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,19 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
+  const arnieQuotesList = [];
   // TODO: Implement this function.
+  for (const url of urls) {
+    const response = await httpGet(url);
+    const { status, body } = response;
+    if (status === 200) {
+      arnieQuotesList.push({ "Arnie Quote": `${JSON.parse(body).message}` });
+    } else {
+      arnieQuotesList.push({ FAILURE: `${JSON.parse(body).message}` });
+    }
+  }
   // return results;
+  return arnieQuotesList;
 };
 
 module.exports = {
