af8f5fb5935c02e289324e365d86f2f6cd312f30
StevenLiu <stevenliu986@gmail.com>
2023-11-02 19:06:50 +1100

    optimise function execution time

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,8 +3,9 @@ const { httpGet } = require("./mock-http-interface");
 const getArnieQuotes = async (urls) => {
   const arnieQuotesList = [];
   // TODO: Implement this function.
-  for (const url of urls) {
-    const response = await httpGet(url);
+  const arniePromises = urls.map((url) => httpGet(url));
+  const responses = await Promise.all(arniePromises);
+  for (const response of responses) {
     const { status, body } = response;
     if (status === 200) {
       arnieQuotesList.push({ "Arnie Quote": `${JSON.parse(body).message}` });
