1780e00e217113c53cf03c240cfcf081ce6f4523
StevenLiu <stevenliu986@gmail.com>
2023-11-02 22:24:55 +1100

    add comments

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,11 @@
 const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
+  // generate a promise array
   const arnieQuotesPromises = urls.map(async (url) => {
     const { status, body } = await httpGet(url);
+
+    // return an object as per the http response status code
     return status === 200
       ? { "Arnie Quote": `${JSON.parse(body).message}` }
       : { FAILURE: `${JSON.parse(body).message}` };
